4c5c61a82192de78bceb7bbdfdcddb879ef15b11
parthergk <gauravkumar81464@gmail.com>
2025-09-21 17:18:34 +0530

    created all route for test

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import * as RecurringSlot from "./models/recurring_slot_Model";
+import * as ExectionSlot from "./models/exection_slot_Model";
 
 dotenv.config();
 
@@ -30,6 +31,34 @@ app.post("/", async (req: Request, res: Response) => {
   try {
     const slots = await RecurringSlot.getSlot(startDate, endDate);
     res.status(200).json(slots);
+  } catch (error) {
+    const errMsg =
+      error instanceof Error
+        ? error.message
+        : "server side error please try again";
+    res.status(400).json({ error: errMsg });
+  }
+});
+
+app.put("/", async (req: Request, res: Response) => {
+  const { id, data } = req.body;
+  try {
+    const updatedSlot = ExectionSlot.updateSlot(id, data);
+    res.status(200).json(updatedSlot);
+  } catch (error) {
+    const errMsg =
+      error instanceof Error
+        ? error.message
+        : "server side error please try again";
+    res.status(400).json({ error: errMsg });
+  }
+});
+
+app.delete("/", async (req: Request, res: Response) => {
+  const { id } = req.body;
+  try {
+    await ExectionSlot.deleteSlot(id);
+    res.status(200).json({ message: "slot deleted" });
   } catch (error) {
     const errMsg =
       error instanceof Error
